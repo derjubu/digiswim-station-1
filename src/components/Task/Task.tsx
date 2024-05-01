adb6ec9d32ec108dd9492fab82252912306b0990
Julian Buchholz <github@julianbuchholz.com>
2024-05-01 11:20:00 +0200

    add: task as hint

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,9 +1,18 @@
 import type { ReactNode } from 'react'
 import classes from './Task.module.css'
+import clsx from 'clsx'
 
 type ButtonProps = {
+  isHint: boolean
   children: ReactNode
 }
-export default function Task({ children }: ButtonProps): JSX.Element {
-  return <p className={classes.description}>{children}</p>
+export default function Task({
+  children,
+  isHint = false,
+}: ButtonProps): JSX.Element {
+  return (
+    <p className={clsx(classes.description, isHint && classes.isHint)}>
+      {children}
+    </p>
+  )
 }
